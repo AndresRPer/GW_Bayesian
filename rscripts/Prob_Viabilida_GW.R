edbe11d7a7a8b81a92f167a7dcb4c4e22f772f53
AndresRPer <arodr218@itam.mx>
2024-06-06 14:56:50 -0600

    Probabilidad de Viabilidad

--- rscripts/Prob_Viabilida_GW.R
+++ rscripts/Prob_Viabilida_GW.R
@@ -57,22 +57,24 @@ for (gen in 1:generations) {
   }
 }
 
-alpha_prior <- array(0.2, dim = c(M, M, max_kappa + 1))  # Prior parameters for Dirichlet distribution
+alpha_prior <- array(1, dim = c(M, M, max_kappa + 1))  # Prior parameters for Dirichlet distribution
 
 # Run the MCMC using MCmultinomialdirichlet
 samples_list <- list()
 for (i in 1:M) {
   for (j in 1:M) {
-    samples_list[[paste(i, j, sep = "_")]] <- MCmultinomdirichlet(
+    samples <- MCmultinomdirichlet(
       counts[i, j, 1:(max_kappa + 1)], 
       alpha_prior[i, j, 1:(max_kappa + 1)], 
       mcmc = 10000, burnin = 1000, thin = 10
     )
+    samples_list[[paste(i, j, sep = "_")]] <- samples
   }
 }
 
 # Combine samples into an array
-samples_array <- array(0, dim = c(1000, M, M, max_kappa + 1))
+num_samples <- nrow(samples_list[[1]])
+samples_array <- array(0, dim = c(num_samples, M, M, max_kappa + 1))
 for (i in 1:M) {
   for (j in 1:M) {
     samples_array[, i, j, 1:(max_kappa + 1)] <- samples_list[[paste(i, j, sep = "_")]]
@@ -83,7 +85,7 @@ for (i in 1:M) {
 P_estimated <- apply(samples_array, c(2, 3, 4), mean)
 
 cat("Estimated Offspring Distribution Matrix P:\n")
-
+print(P_estimated)
 
 # Rearrange P_estimated into three matrices of size M x (max_kappa + 1)
 P_rearranged <- list()
@@ -95,53 +97,60 @@ for (i in 1:M) {
     }
   }
 }
-print(P_rearranged)
-
-# Calculate the MSE for each parent type
-mse_values <- numeric(M)
-for (i in 1:M) {
-  mse_values[i] <- mean((P_rearranged[[i]] - P_true_list[[i]])^2)
-}
-
-# Print the MSE values
-for (i in 1:M) {
-  cat(sprintf("MSE for Parent Type %d: %f\n", i, mse_values[i]))
-}
 
 # Calculate the mean matrix
-mean_matrix <- matrix(0, nrow = M, ncol = M)
-for (i in 1:M) {
-  for (j in 1:M) {
-    mean_matrix[i, j] <- sum((0:max_kappa) * P_rearranged[[i]][j, ])
+calculate_mean_matrix <- function(P_matrix, max_kappa) {
+  mean_matrix <- matrix(0, nrow = nrow(P_matrix), ncol = nrow(P_matrix))
+  for (i in 1:nrow(P_matrix)) {
+    for (j in 1:nrow(P_matrix)) {
+      mean_matrix[i, j] <- sum((0:max_kappa) * P_matrix[j, ])
+    }
+  }
+  return(mean_matrix)
+}
+
+# Simulate the process and estimate super-criticality probability
+m <- 1000  # Number of simulations
+super_critical_count <- 0
+
+for (sim in 1:m) {
+  # Simulate d^2 posterior offspring laws
+  P_simulated <- list()
+  for (i in 1:M) {
+    P_simulated[[i]] <- matrix(0, nrow = M, ncol = max_kappa + 1)
+    for (j in 1:M) {
+      sample_idx <- sample(1:num_samples, 1)  # Sample from posterior
+      P_simulated[[i]][j, ] <- samples_array[sample_idx, i, j, ]
+    }
+  }
+  
+  # Calculate the mean matrix for the simulated P
+  mean_matrix_sim <- matrix(0, nrow = M, ncol = M)
+  for (i in 1:M) {
+    mean_matrix_sim <- mean_matrix_sim + calculate_mean_matrix(P_simulated[[i]], max_kappa)
+  }
+  mean_matrix_sim <- mean_matrix_sim / M
+  
+  # Calculate the largest eigenvalue
+  eigenvalues_sim <- eigen(mean_matrix_sim)$values
+  largest_eigenvalue_sim <- max(Re(eigenvalues_sim))
+  
+  # Check if the largest eigenvalue is greater than 1
+  if (largest_eigenvalue_sim > 1) {
+    super_critical_count <- super_critical_count + 1
   }
 }
 
-cat("Mean Matrix:\n")
-print(mean_matrix)
-
-# Determine the largest eigenvalue of the mean matrix
-eigenvalues <- eigen(mean_matrix)$values
-largest_eigenvalue <- max(Re(eigenvalues))
-
-# Determine if the process is critical, sub-critical, or super-critical
-if (largest_eigenvalue < 1) {
-  process_type <- "Sub-critical"
-} else if (largest_eigenvalue > 1) {
-  process_type <- "Super-critical"
-} else {
-  process_type <- "Critical"
-}
-
-cat("Process type based on the largest eigenvalue of the mean matrix: ", process_type, "\n")
-
+# Calculate the probability of super-criticality
+prob_super_critical <- super_critical_count / m
+cat("Estimated Probability of Super-Criticality: ", prob_super_critical, "\n")
 
 # Prepare P_true for comparison
 P_true_extended <- list()
 for (i in 1:M) {
   P_true_extended[[i]] <- matrix(0, nrow = M, ncol = max_kappa + 1)
   for (j in 1:M) {
-    P_true_extended[[i]][j, 1] <- P_true_list[[i]][j, 1]
-    P_true_extended[[i]][j, 2:(max_kappa + 1)] <- P_true_list[[i]][j, 2:(max_kappa + 1)]
+    P_true_extended[[i]][j, 1:(max_kappa + 1)] <- P_true_list[[i]][j, 1:(max_kappa + 1)]
   }
 }
 
@@ -174,45 +183,9 @@ for (i in 1:M) {
 comparison_df <- rbind(P_estimated_df, P_true_df)
 
 # Plot the comparison
-ggplot(comparison_df, aes(x = factor(OffspringType), y = Probability, fill = Type)) +
+ggplot(comparison_df, aes(x = factor(OffspringCount), y = Probability, fill = Type)) +
   geom_bar(stat = "identity", position = "dodge") +
   facet_wrap(~ ParentType, labeller = label_both) +
-  labs(x = "Tipo de Descendiente", y = "Probabilidad", 
-       title = "Comparación de la ley de reproducción estimada y eerdadera") +
+  labs(x = "Número de Descendientes", y = "Probabilidad", 
+       title = "Comparación de la Distribución de Descendencia Estimada y Verdadera") +
   theme_minimal()
-
-# Simulate the process and estimate super-criticality probability
-m <- 1000  # Number of simulations
-super_critical_count <- 0
-
-for (sim in 1:m) {
-  # Simulate d^2 posterior offspring laws
-  P_simulated <- list()
-  for (i in 1:M) {
-    P_simulated[[i]] <- matrix(0, nrow = M, ncol = max_kappa + 1)
-    for (j in 1:M) {
-      sample_idx <- sample(1:900, 1)  # Sample from posterior
-      P_simulated[[i]][j, ] <- samples_array[sample_idx, i, j, ]
-    }
-  }
-  
-  # Calculate the mean matrix for the simulated P
-  mean_matrix_sim <- matrix(0, nrow = M, ncol = M)
-  for (i in 1:M) {
-    mean_matrix_sim <- mean_matrix_sim + calculate_mean_matrix(P_simulated[[i]], max_kappa)
-  }
-  mean_matrix_sim <- mean_matrix_sim / M
-  
-  # Calculate the largest eigenvalue
-  eigenvalues_sim <- eigen(mean_matrix_sim)$values
-  largest_eigenvalue_sim <- max(Re(eigenvalues_sim))
-  
-  # Check if the largest eigenvalue is greater than 1
-  if (largest_eigenvalue_sim > 1) {
-    super_critical_count <- super_critical_count + 1
-  }
-}
-
-# Calculate the probability of super-criticality
-prob_super_critical <- super_critical_count / m
-cat("Estimated Probability of Super-Criticality: ", prob_super_critical, "\n")
